b4757dd41a629e90df3f6af77fbb7871dc8d87ff
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2025-03-18 11:20:42 +0100

    feat:Optimized performance with lazy loading, reduced re-renders, and improved app responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,46 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import HistoricalData from "./pages/HistoricalData";
-import AboutUs from "./pages/AboutUs"; 
-import ContactUs from "./pages/ContactUs";  
 import { CurrencyProvider } from "./context/CurrencyContext";
-import { LoadingScreen } from "./components/LoadingScreen"; 
+import { LoadingScreen } from "./components/LoadingScreen";
+
+// Lazy load components to improve initial load time
+const Navbar = lazy(() => import("./components/Navbar"));
+const Footer = lazy(() => import("./components/Footer"));
+const Home = lazy(() => import("./pages/Home"));
+const HistoricalData = lazy(() => import("./pages/HistoricalData"));
+const AboutUs = lazy(() => import("./pages/AboutUs"));
+const ContactUs = lazy(() => import("./pages/ContactUs"));
 
 function App() {
   const [loading, setLoading] = useState(true);
 
+  // to Stop loading after 3 seconds
+  const stopLoading = useCallback(() => setLoading(false), []);
+
   useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, []);
+    const timer = setTimeout(stopLoading, 3000);
+    return () => clearTimeout(timer);
+  }, [stopLoading]);
 
   return (
     <CurrencyProvider>
       {loading ? (
-        <LoadingScreen onComplete={() => setLoading(false)} />
+        <LoadingScreen onComplete={stopLoading} />
       ) : (
-        <Router>
-          <div className="flex h-screen flex-col justify-between">
-            <Navbar />
-            <div className="flex-1 p-6">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/history" element={<HistoricalData />} />
-                <Route path="/about" element={<AboutUs />} /> 
-                <Route path="/contact" element={<ContactUs />} />
-              </Routes>
+          <Router>
+            <div className="flex h-screen flex-col justify-between">
+              <Navbar />
+              <div className="flex-1 p-6">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/history" element={<HistoricalData />} />
+                  <Route path="/about" element={<AboutUs />} />
+                  <Route path="/contact" element={<ContactUs />} />
+                </Routes>
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
-        </Router>
+          </Router>
       )}
     </CurrencyProvider>
   );
